51f5e6b4597625a7b8d1254f3dec8ea4ea08a6b7
Fatmagül Demirbaş <dfatmagul72@gmail.com>
2022-09-21 22:02:43 +0300

    değişkenler oluşturuldu

--- BirthdayApp/ViewController.swift
+++ BirthdayApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var UserNameText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func ButtonClicked(_ sender: Any) {
+    }
 }
 
